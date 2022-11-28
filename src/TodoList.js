b58c2c40bad1a8f8c22c222729fbd3c4bd8c36e0
zeze <89672780+mdmdr8@users.noreply.github.com>
2022-11-28 16:15:36 +0900

    오늘 할 일 count
    count To Do List

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,30 @@ import { useState } from "react";
 
 function TodoList() {
     const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
 
     const onChange = (e) => {
         setTodo(e.target.value);
     };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (todo === '') {
+            return;
+        }
+        setTodo("");
+        setTodos((currentArray) => [todo, ...currentArray])
+    };
+
     return (
         <div>
-            <h1>오늘 할 일</h1>
-            <form>
+            <h1>오늘 할 일[{todos.length}]</h1>
+            <form onSubmit={onSubmit} >
                 <input onChange={onChange} value={todo} type="text" placeholder="Write your to do..." />
                 &nbsp;&nbsp;
                 <button> Add To Do</button>
-                <hr />
             </form>
+            <hr />
         </div>
     )
 }
